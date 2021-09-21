b854c2b6d0cd0e8805e9ab17996d7861330ba41b
zhulym <zhulym@mail.ru>
2021-09-21 17:57:39 +0300

    feat: add form in tickets, impl ripple for button

--- museum/script/tickets.js
+++ museum/script/tickets.js
@@ -1,36 +1,30 @@
 const ticketsBuyBtn = document.querySelector('.tickets__buy');
+const formBookBtn = document.querySelector('.card-form-submit');
 const ticketsOverlay = document.querySelector('.tickets__overlay');
 const ticketsFormItem = document.querySelector('.tickets__form-item');
 const closeBtn = document.querySelector('.close-form');
 
-
-function createRipple(event) {
-  const button = event.currentTarget;
-  const circle = document.createElement("span");
-  const diameter = Math.max(button.clientWidth, button.clientHeight);
-  const radius = diameter / 2;
-  circle.style.width = circle.style.height = `${diameter}px`;
-  circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
-  circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
-  circle.classList.add("ripple");
-  const ripple = button.querySelector(".ripple");
-  if (ripple) {
-    ripple.remove();
-  }
-  button.appendChild(circle);
-}
-function openTicketsForm() {
-  ticketsOverlay.classList.add('active');
-  setTimeout(() => { ticketsFormItem.classList.add('form-animation') }, 300)
-
-
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   ticketsBuyBtn.addEventListener("click", openTicketsForm);
+  formBookBtn.addEventListener('click', (e) => e.preventDefault());
+  formBookBtn.addEventListener("click", createRipple);
   ticketsFormItem.addEventListener("click", (e) => e.stopPropagation());
   [ticketsOverlay, closeBtn].forEach(el => el.addEventListener("click", () => {
     ticketsOverlay.classList.remove('active');
     ticketsFormItem.classList.remove('form-animation');
   }))
 });
+
+function openTicketsForm() {
+  ticketsOverlay.classList.add('active');
+  setTimeout(() => { ticketsFormItem.classList.add('form-animation') }, 300)
+}
+
+function createRipple(e) {
+  const circle = document.createElement('span')
+  circle.classList.add('circle')
+  circle.style.top = e.offsetY + 'px'
+  circle.style.left = e.offsetX + 'px'
+  this.appendChild(circle)
+  setTimeout(() => circle.remove(), 500)
+}
